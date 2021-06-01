013341d9c20f6922694e43b717137e0b81c1f96b
Paolo Gomez <78209142+pattherogue@users.noreply.github.com>
2021-06-01 08:55:18 -0700

    JS

--- app.js
+++ app.js
@@ -8,4 +8,30 @@ const modalContainer = document.querySelector(".modal-content");
 const modalCLose = document.querySelector(".modal-close");
 
 //fetch data from API
+fetch(urAPI)
+    .then(res => res.json())
+    .then(res => res.results)
+    .then(displayEmployees)
+    .catch(err => console.log(err))
 
+    function displayEmployee(employeeData) {
+
+        employees = employeeData;
+
+        // stpre the employee HTML created 
+        let employeeHTML = '';
+
+        // loop through each employee and create HTML markup
+        employees.forEach(employee, index) => {
+            let name = employee.name;
+            let email = employee.email;
+            let city = employee.location.city;
+            let pictre = employee.picture;
+
+            employeeHTML += `
+            
+            
+            `
+            }
+        }
+    }
